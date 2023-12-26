031733dcb30926d158f43c6ec339b4bb4078e828
josephin09 <josephindavidlatha@gmail.com>
2023-12-26 12:12:41 +0530

    stateupdate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedStatement, setSelectedStatement] = useState("");
+
+  const handleCountryChange = (e) => {
+    setSelectedCountry(e.target.value);
+    setStates([]);
+    setCities([]);
+    setSelectedStatement("");
+  };
+
+  const handleStateChange = (e) => {
+    setSelectedState(e.target.value);
+    setCities([]);
+    setSelectedStatement("");
+  };
+
+  const handleCityChange = (e) => {
+    setSelectedCity(e.target.value);
+    setSelectedStatement("");
+  };
+
+  const getCountriesData = async () => {
+    try {
+      const data = await fetch(
+        "https://crio-location-selector.onrender.com/countries"
+      );
+      const res = await data.json();
+      setCountries(res);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getStatesData = async () => {
+    try {
+      const data = await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectedCountry}/states`
+      );
+      const res = await data.json();
+      setStates(res);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getCitiesData = async () => {
+    try {
+      const data = await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
+      );
+      const res = await data.json();
+      setCities(res);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const generateSelectedStatement = () => {
+    if (selectedCountry && selectedState && selectedCity) {
+      const statement = (
+        <>
+          You selected{" "}
+        <span className="bold-text">{selectedCity}</span>,{" "}
+        <span className="dull-text">{selectedState}</span>,{" "}
+        <span className="dull-text">{selectedCountry}</span>
+        </>
+      );
+      setSelectedStatement(statement);
+    }
+  };
+
+  useEffect(() => {
+    getCountriesData();
+  }, []);
+
+  useEffect(() => {
+    if (selectedCountry) {
+      getStatesData();
+    }
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    if (selectedState) {
+      getCitiesData();
+    }
+  }, [selectedState]);
+
+  useEffect(() => {
+    generateSelectedStatement();
+  }, [selectedCountry, selectedState, selectedCity]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Select Location</h1>
+      <div>
+        <select onChange={handleCountryChange} value={selectedCountry}>
+          <option value="" disabled>
+            Select Country
+          </option>
+          {countries.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <select onChange={handleStateChange} value={selectedState}>
+          <option value="" disabled>
+            Select State
+          </option>
+          {states.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <select onChange={handleCityChange} value={selectedCity}>
+          <option value="" disabled>
+            Select City
+          </option>
+          {cities.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      </div>
+      <p>{selectedStatement}</p>
     </div>
   );
 }
-
-export default App;
